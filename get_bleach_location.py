912720be3fddf96c5f96e3854130ae4d51aa18bf
Nico Stuurman <nico.stuurman@ucsf.edu>
2020-12-21 09:36:21 -0800

    Small progress in bleach location

--- get_bleach_location.py
+++ get_bleach_location.py
@@ -13,14 +13,19 @@ mmc = bridge.get_core()
 mm = bridge.get_studio()
 projector = bridge.construct_java_object("org.micromanager.projector.ProjectorAPI")
 projector_device = projector.get_projection_device()
-mapping = projector.load_mapping(projector_device)
-point_java = bridge.construct_java_object("java.awt.geom.Point2D.Double")
+
 
 p_exposure = projector_device.get_exposure()
+
 img = mm.live().snap(False).get(0)
 pixels = np.reshape(img.get_raw_pixels(), newshape=[img.get_height(), img.get_width()])
 
-point_java = bridge.construct_java_object("java.awt.geom.Point2D.Double")
-point = projector.transformPoint(mapping, point_java, null, 1)
+img = mm.live().snap(False).get(0)
+pixels = np.reshape(img.get_raw_pixels(), newshape=[img.get_height(), img.get_width()])
+
+projector.enable_point_and_shoot_mode(True)
 projector_device.set_exposure(400)
-projector_device.display_spot(point_java.x, point_java.y)
+projector.add_point_to_point_and_shoot_queue(shot['centroid'][1], shot['centroid'][0])
+img = mm.live().snap(False).get(0)
+pixels2 = np.reshape(img.get_raw_pixels(), newshape=[img.get_height(), img.get_width()])
+
