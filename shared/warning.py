672c2880ff8d561a99735a93e5b35b8660a82924
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-01-27 14:01:56 -0800

    solved some warnnings

--- shared/warning.py
+++ shared/warning.py
@@ -11,14 +11,3 @@ def check_img_supported(img):
     """
     if not isinstance(img, np.ndarray):
         raise TypeError("Only integer image types are supported. Got %s." % type(img))
-
-
-def check_input_supported(inp, supported):
-    """
-    Check if input value is in supported value range.  Raise value error if not.
-
-    :param inp: All, input value
-    :param supported: list, supported input range
-    """
-    if not inp in supported:
-        raise ValueError("Only accepts %s. Got %s" % (supported, inp))
