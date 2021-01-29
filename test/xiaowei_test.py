aaf0886e620daf82d6a4d6e3641a017fa9a52d07
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-01-29 00:35:17 -0800

    random test

--- test/xiaowei_test.py
+++ test/xiaowei_test.py
@@ -1,40 +1,38 @@
-# --------------------------
-# IMPORTS
-# --------------------------
 import numpy as np
 import pandas as pd
 import napari
 from pycromanager import Bridge
-
 from matplotlib.backends.qt_compat import QtCore, QtWidgets
 if QtCore.qVersion() >= "5.":
-    from matplotlib.backends.backend_qt5agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+    from matplotlib.backends.backend_qt5agg import FigureCanvas
 else:
-    from matplotlib.backends.backend_qt4agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
-
+    from matplotlib.backends.backend_qt4agg import FigureCanvas
 from matplotlib.figure import Figure
 from vispy.color import Colormap
-import collections
-
-from skimage.measure import label, regionprops
-
-from shared.find_organelles import find_nucleoli
+from skimage.measure import label
+from shared.find_organelles import find_organelle, nucleoli_analysis
 import shared.analysis as ana
 import shared.display as dis
-import shared.dataframe as dat
 import shared.objects as obj
+import shared.bleach_points as ble
+from matplotlib import pyplot as plt
 
 # --------------------------
 # PARAMETERS
 # --------------------------
 # data source
-# data_path = "C:\\Users\\NicoLocal\\Images\\Jess\\20201116-Nucleoili-bleaching-4x\\PythonAcq1\\AutoBleach_15"
-data_path = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/TestedData/20201216/Ctrl-2DG-CCCP-36pos_partial/exp_37"
-
+# data_path = "C:\\Users\\NicoLocal\\Images\\Jess\\20201116-Nucleoli-bleaching-4x\\PythonAcq1\\AutoBleach_15"
+# data_path = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/AutoBleach_15"
+data_path = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/" \
+            "20201216_CBB_nucleoliBleachingTest_drugTreatment/Ctrl-2DG-CCCP-36pos_partial/exp_112/"
 # values
-thresholding = 'local'  # global thresholding method; choose in between 'na','otsu' and 'yen'; default = 'na'
+data_z = 0
+data_c = 0
+data_p = 0
+ref_t = 0
+thresholding = 'local-nucleoli'
+mode_bleach_detection = 'single-raw'
+# global thresholding method; choose in between 'na','otsu','yen', 'local-nucleoli' and 'local-sg'
 min_size = 10  # minimum nucleoli size; default = 10
 max_size = 1000  # maximum nucleoli size; default = 1000;
                  # larger ones are generally cells without nucleoli
@@ -63,31 +61,40 @@ max_t = store.get_max_indices().get_t()
 cb = mm.data().get_coords_builder()
 cb.t(0).p(0).c(0).z(0)
 
-# ------------------------------
-# IMAGE ANALYSIS based on time 0
-# ------------------------------
-print("### Image analysis based on time 0 ...")
+# --------------------------------------
+# IMAGE ANALYSIS based on reference time
+# --------------------------------------
+print("### Image analysis based on reference time %s ..." % ref_t)
 
-# test image of time 0
-t0 = store.get_image(cb.t(0).build())
-t0_pix = np.reshape(t0.get_raw_pixels(), newshape=[t0.get_height(), t0.get_width()])
+# reference image of ref_t
+temp = store.get_image(cb.p(data_p).z(data_z).c(data_c).t(ref_t).build())
+pix = np.reshape(temp.get_raw_pixels(), newshape=[temp.get_height(), temp.get_width()])
 
 # find nucleoli
-nucleoli = find_nucleoli(t0_pix, thresholding, min_size=min_size, max_size=max_size)
+nucleoli = find_organelle(pix, thresholding, min_size=min_size, max_size=max_size)
 print("Found %d nucleoli." % obj.object_count(nucleoli))
 
-# --------------------------
-# OUTPUT DISPLAY
-# --------------------------
-print("### Output display ...")
-with napari.gui_qt():
-    # embed mpl widget in napari viewer
-    mpl_widget = FigureCanvas(Figure(figsize=(5, 3)))
-    [ax1, ax2] = mpl_widget.figure.subplots(nrows=1, ncols=2)
-    viewer = napari.Viewer()
-    viewer.window.add_dock_widget(mpl_widget)
+# nucleoli pd dataset
+nucleoli_pd = nucleoli_analysis(nucleoli)
 
-    viewer.add_image(t0_pix, name='data')
+# ----------------------------------
+# BLEACH SPOTS DETECTION
+# ----------------------------------
+print("### Bleach spots detection ...")
 
-    viewer.add_image(nucleoli, name='nucleoli', contrast_limits=[0, 1],
-                     colormap=('violet woBg', violet_woBg))
+# load point_and_shoot log file
+log_pd = pd.read_csv('%s/PointAndShoot.log' % data_path, na_values=['.'], sep='\t', header=None)
+print("Aim to photobleach %d spots." % len(log_pd))
+log_pd = ble.get_bleach_frame(log_pd, store, cb)
+
+# get bleach spot coordinate
+log_pd = ble.get_bleach_spots_coordinates(log_pd, store, cb, mode_bleach_detection)
+
+# generate bleach spot mask
+bleach_spots, pointer_pd = ble.get_bleach_spots(log_pd, nucleoli, nucleoli_pd, store, cb, num_dilation)
+print("%d spots passed filter for analysis." % obj.object_count(bleach_spots))
+
+plt.subplots(figsize=(8, 6))
+for i in range(len(pointer_pd)):
+    plt.plot(pointer_pd['mean_int'][i])
+plt.savefig('%s/test.pdf' % data_path)
