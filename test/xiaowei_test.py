caa9755bd3e02c19a7fc46e54f819610129226f6
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-01-30 02:36:04 -0800

    random test

--- test/xiaowei_test.py
+++ test/xiaowei_test.py
@@ -9,13 +9,16 @@ else:
     from matplotlib.backends.backend_qt4agg import FigureCanvas
 from matplotlib.figure import Figure
 from vispy.color import Colormap
-from skimage.measure import label
+from skimage.measure import label,regionprops
 from shared.find_organelles import find_organelle, nucleoli_analysis
 import shared.analysis as ana
 import shared.display as dis
 import shared.objects as obj
 import shared.bleach_points as ble
 import os
+from skimage.segmentation import random_walker, clear_border
+from scipy import ndimage
+
 
 # --------------------------
 # PARAMETERS allow change
@@ -23,7 +26,7 @@ import os
 # paths
 # data_path = "C:\\Users\\NicoLocal\\Images\\Jess\\20201116-Nucleoli-bleaching-4x\\PythonAcq1\\AutoBleach_15"
 data_path = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/" \
-            "20201216_CBB_nucleoliBleachingTest_drugTreatment/Ctrl-2DG-CCCP-36pos_partial/exp_110/"
+            "20201216_CBB_nucleoliBleachingTest_drugTreatment/Ctrl-2DG-CCCP-36pos_partial/exp_98/"
 save_path = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/" \
             "20201216_CBB_nucleoliBleachingTest_drugTreatment/Ctrl-2DG-CCCP-36pos_partial/exp_110/"
 
@@ -68,9 +71,25 @@ print("### Image analysis: nucleoli detection based on reference time %s ..." % 
 # reference image of ref_t
 temp = store.get_image(cb.p(data_p).z(data_z).c(data_c).t(ref_t).build())
 pix = np.reshape(temp.get_raw_pixels(), newshape=[temp.get_height(), temp.get_width()])
-# nucleoli detection
-nucleoli = find_organelle(pix, thresholding, min_size=min_size, max_size=max_size)
-print("Found %d nucleoli." % obj.object_count(nucleoli))
-# nucleoli pd dataset
-nucleoli_pd = nucleoli_analysis(nucleoli)
-print(nucleoli_pd[:5])
+# nuclear detection
+markers = np.zeros_like(pix)
+markers[pix < 220] = 1
+markers[pix > 450] = 2
+seg = random_walker(pix, markers)
+
+nuclear = np.zeros_like(pix)
+nuclear[seg == 2] = 1
+nuclear_fill = ndimage.binary_fill_holes(nuclear)
+
+# separate touching nuclei
+label_nuclear = obj.label_watershed(nuclear_fill)
+label_nuclear_ft = clear_border(label_nuclear)
+label_nuclear_ft = obj.label_remove_small(label_nuclear_ft, 1500)
+
+nuclear_prop = regionprops(label_nuclear_ft)
+
+with napari.gui_qt():
+    viewer = napari.Viewer()
+    viewer.add_image(pix, name='data')
+    winter_woBg = dis.num_color_colormap('winter', len(nuclear_prop))[0]
+    viewer.add_image(label_nuclear_ft, name='nuclear', colormap=('winter woBg', winter_woBg))
