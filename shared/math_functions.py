3e0b3c26570be54963896faecc27e4278c346160
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-01-31 19:30:03 -0800

    add comments

--- shared/math_functions.py
+++ shared/math_functions.py
@@ -3,6 +3,8 @@
 # ------------------------------------------
 
 import numpy as np
+from scipy.optimize import curve_fit
+import pandas as pd
 
 
 # single exponential
@@ -51,3 +53,72 @@ def single_exp_decay(x, a, b):
     :param b:
     """
     return 1 - a * (1 - np.exp(-b * x))
+
+
+def bg_fitting_linear(bg_int_tseries):
+
+    try:
+        popt, _ = curve_fit(linear, np.arange(0, len(bg_int_tseries), 1), bg_int_tseries)
+        a, b = popt
+    except RuntimeError:
+        a = np.nan
+        b = np.nan
+
+    bg_fit = []
+    for j in range(len(bg_int_tseries)):
+        bg_fit.append(linear(j, a, b))
+    r2 = r_square(bg_int_tseries, bg_fit)
+
+    return bg_fit, r2, a, b
+
+
+def pb_factor_fitting_single_exp(pb_factor_tseries):
+    try:
+        popt, _ = curve_fit(single_exp_decay, np.arange(0, len(pb_factor_tseries), 1), pb_factor_tseries)
+        a, b = popt
+    except RuntimeError:
+        a = np.nan
+        b = np.nan
+
+    pb_fit = []
+    for j in range(len(pb_factor_tseries)):
+        pb_fit.append(single_exp_decay(j, a, b))
+    r2 = r_square(pb_factor_tseries, pb_fit)
+
+    return pb_fit, r2, a, b
+
+
+def frap_fitting_single_exp(time_tseries_lst: list, frap_lst: list):
+    single_exp_a = []
+    single_exp_b = []
+    single_exp_fit = []
+    single_exp_r2 = []
+    single_exp_t_half = []
+
+    for i in range(len(frap_lst)):
+        try:
+            popt, _ = curve_fit(single_exp, time_tseries_lst[i], frap_lst[i])
+            a, b = popt
+        except RuntimeError:
+            a = np.nan
+            b = np.nan
+
+        y_fit = []
+        for j in range(len(time_tseries_lst[i])):
+            y_fit.append(single_exp(time_tseries_lst[i][j], a, b))
+        r2 = r_square(frap_lst[i], y_fit)
+        t_half_fit = np.log(0.5) / (-b)
+        single_exp_a.append(a)
+        single_exp_b.append(b)
+        single_exp_fit.append(y_fit)
+        single_exp_r2.append(r2)
+        single_exp_t_half.append(t_half_fit)
+
+    fit_pd = pd.DataFrame({'single_exp_fit': single_exp_fit,
+                           'single_exp_r2': single_exp_r2,
+                           'single_exp_a': single_exp_a,
+                           'single_exp_b': single_exp_b,
+                           'single_exp_mobile_fraction': single_exp_a,
+                           'single_exp_t_half': single_exp_t_half})
+
+    return fit_pd
