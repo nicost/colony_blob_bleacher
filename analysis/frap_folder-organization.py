13da1c86a1fe29ab61589265210ad0bb6ee854f2
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-02-28 23:43:37 -0800

    write a file to organize folder automatically

---
+++ analysis/frap_folder-organization.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+data_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Data/" \
+            "20210226_CBB_nucleoliWTPhotobleachingConditionTest/10mW_50ms/"
+
+dirs = [x[0] for x in os.walk(data_source)]
+dirs.pop(0)
+num_dir = len(dirs)
+
+for s in range(len(dirs)):
+    data_path = dirs[s]
+    mf_name = dirs[s].split('/')[-1].split('-')[0]
+    move_path = ("%s/%s/" % (data_source, mf_name))
+    if not os.path.exists(move_path):
+        os.makedirs(move_path)
+    shutil.move(dirs[s], move_path)
+
+print("Done")
