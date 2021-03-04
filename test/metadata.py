71afb632ae00902449ca6c3412e8c6b2946ab80b
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local.ucsf.edu>
2021-03-04 15:17:25 -0800

    tried a couple things, haven't worked

--- test/metadata.py
+++ test/metadata.py
@@ -1,9 +1,19 @@
 from tifffile import TiffFile
+from pycromanager import Bridge
 
 data_path = '/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Data/test/' \
             'D2-Site_3_1/D2-Site_3_1_MMStack_Default.ome.tif'
+data_path1 = '/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Data/test/data/D2/D2-Site_1_1'
 
-with TiffFile(data_path) as tif:
-    img_metadata = tif.imagej_metadata
+#with TiffFile(data_path) as tif:
+#    img_metadata = tif.imagej_metadata
 
-print(img_metadata)
+#print(img_metadata)
+
+# build up pycromanager bridge
+bridge = Bridge()
+mm = bridge.get_studio()
+
+ds = mm.displays().get_active_data_viewer().get_data_provider(data_path1)
+
+print(ds)
