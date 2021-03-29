6f0da7f32692ba7bdb65d66b8e6b0757b92b06eb
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-03-29 12:30:38 -0700

    assign ep-rc before if-else to make sure it is assigned for every branch

--- shared/analysis.py
+++ shared/analysis.py
@@ -82,6 +82,7 @@ def bleach_location(pre_pixels: np.array,
     :return:
     """
     # assume pre and post are the same size
+    ep_rc = expected_position
     if expected_position == DEFAULT or half_roi_size == DEFAULT:
         pre_roi = pre_pixels
         post_roi = post_pixels
@@ -95,9 +96,11 @@ def bleach_location(pre_pixels: np.array,
         else:
             cc = post_pixels.shape[1] - expected_position[1]
             ep_rc = [expected_position[0], cc]
-            pre_roi = pre_pixels[ep_rc[0] - half_roi_size[0]:ep_rc[0] + half_roi_size[0],
+            pre_roi = pre_pixels[
+                      ep_rc[0] - half_roi_size[0]:ep_rc[0] + half_roi_size[0],
                       ep_rc[1] - half_roi_size[1]:ep_rc[1] + half_roi_size[1]]
-            post_roi = post_pixels[ep_rc[0] - half_roi_size[0]:ep_rc[0] + half_roi_size[0],
+            post_roi = post_pixels[
+                       ep_rc[0] - half_roi_size[0]:ep_rc[0] + half_roi_size[0],
                        ep_rc[1] - half_roi_size[1]:ep_rc[1] + half_roi_size[1]]
     subtracted = post_roi + 100 - pre_roi
     selem = disk(2)
