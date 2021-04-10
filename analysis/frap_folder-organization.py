b3d0aca6cac48e454a779a29ab42f29434030e00
xwyan1230 <xwyan1230@gmail.com>
2021-04-10 14:38:26 -0700

    fold organization optimization

--- analysis/frap_folder-organization.py
+++ analysis/frap_folder-organization.py
@@ -1,8 +1,7 @@
 import os
 import shutil
 
-data_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Data/" \
-                "20210302_CBB_nucleoliWTcellDensityTest/100k"
+data_source = "D:/Xiaowei/data/20210407_SG_frap/"
 
 dirs = [x[0] for x in os.walk(data_source)]
 dirs.pop(0)
@@ -11,7 +10,7 @@ num_dir = len(dirs)
 for s in range(len(dirs)):
     data_path = dirs[s]
     mf_name = dirs[s].split('/')[-1].split('-')[0]
-    move_path = ("%s/%s/" % (data_source, mf_name))
+    move_path = ("%s/data/%s/" % (data_source, mf_name))
     if not os.path.exists(move_path):
         os.makedirs(move_path)
     shutil.move(dirs[s], move_path)
