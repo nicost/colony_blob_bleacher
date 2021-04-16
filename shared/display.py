40f6b900fe1c694f5de91096a1c89ef6158d3253
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-04-16 00:06:52 -0700

    modify function plot_minus_ln_p and write new function get_phenotype for frap screen analysis

--- shared/display.py
+++ shared/display.py
@@ -67,11 +67,14 @@ Specific output/display (save space in the main script):
         FUNCTION: create pair-wise x value for -ln(p) plot
         SYNTAX:   get_x(inc: int, limit: int, repeat: int, offset: float)
     
+    get_phenotype
+        FUNCTION: get phenotype value (average -ln(p) value)
+        SYNTAX:   get_phenotype(data1: pd.DataFrame, data2: pd.DataFrame, feature: str, limit: int, repeat: int)
+    
     plot_minus_ln_p
         FUNCTION: generate -ln(p) plot for given feature
-        SYNTAX:   plot_minus_ln_p(inc: int, limit: int, repeat: int, feature: str, pd_WT: pd.DataFrame, 
-                  pd_WT1: pd.DataFrame, pd_WT2: pd.DataFrame, pd_WT3: pd.DataFrame, pd_sample: pd.DataFrame, 
-                  save_path: str, sample_name: str)
+        SYNTAX:   plot_minus_ln_p(inc: int, limit: int, repeat: int, feature: str, data_pd: pd.DataFrame, ctrl_lst: 
+                  list, sample: str, save_path: str)
     
     plot_violin
         FUNCTION: generate violin plot for given feature
@@ -477,9 +480,33 @@ def get_x(inc: int, limit: int, repeat: int, offset: float):
     return out
 
 
-def plot_minus_ln_p(inc: int, limit: int, repeat: int, feature: str, pd_WT: pd.DataFrame, pd_WT1: pd.DataFrame,
-                    pd_WT2: pd.DataFrame, pd_WT3: pd.DataFrame, pd_sample: pd.DataFrame, save_path: str,
-                    sample_name: str):
+def get_phenotype(data1: pd.DataFrame, data2: pd.DataFrame, feature: str, limit: int, repeat: int):
+    """
+    get phenotype value (average -ln(p) value)
+
+    :param data1: pd.DataFrame, data1
+    :param data2: pd.DataFrame, data2
+    :param feature: str, comparing feature, column name
+    :param limit: int, upper limit of the plot
+    :param repeat: int, how many runs to be calculated per condition (generally 50)
+    :return:
+    """
+    pd_data1 = data1 if feature != 'circ' else data1[data1['size'] > 50]
+    pd_data2 = data2 if feature != 'circ' else data2[data2['size'] > 50]
+
+    minimum = np.min([len(pd_data1), len(pd_data2)])
+    limit = minimum if (minimum < limit) else limit
+
+    p_lst = []
+    for j in range(repeat):
+        p = -np.log(ks_2samp(pd_data1[feature].sample(n=limit).tolist(), pd_data2[feature].sample(n=limit).tolist())[1])
+        p_lst.append(p)
+
+    return limit, np.mean(p_lst)
+
+
+def plot_minus_ln_p(inc: int, limit: int, repeat: int, feature: str, data_pd: pd.DataFrame, ctrl_lst: list, sample: str,
+                    save_path: str):
     """
     Generate -ln(p) plot for given feature
 
@@ -487,29 +514,40 @@ def plot_minus_ln_p(inc: int, limit: int, repeat: int, feature: str, pd_WT: pd.D
     :param limit: int, upper limit of the plot
     :param repeat: int, how many runs to be calculated per condition (generally 50)
     :param feature: str, comparing feature, column name
-    :param pd_WT: pd.DataFrame, total wild type sample (generated from multiple wells)
-    :param pd_WT1: pd.DataFrame, negative control WT1
-    :param pd_WT2: pd.DataFrame, negative control WT2
-    :param pd_WT3: pd.DataFrame, negative control WT3
-    :param pd_sample: pd.DataFrame, treatment sample
-    :param save_path: str, save location
-    :param sample_name: str, treatment sample name
+    :param data_pd: pd.DataFrame, total sample
+    :param ctrl_lst: list, list of control wells
+    :param sample: str, sample name
+    :param save_path: str, saving path
     :return:
     """
-    x = np.arange(limit + 5)
+    sample_lst = ctrl_lst + [sample, 'WT']
+    for i in sample_lst:
+        n_curve = len(data_pd[data_pd['sample'] == i]) if feature != 'circ' else \
+            len(data_pd[(data_pd['sample'] == i) & (data_pd['size'] > 50)])
+        if n_curve < limit:
+            limit = n_curve
+
+    x = np.arange(limit+5)
     plt.figure(figsize=(15, 4))
-    plt.scatter(get_x(inc, limit, repeat, -1), get_p(pd_WT1, pd_WT, feature, inc, limit, repeat),
-                alpha=0.5, s=5, c='#40E0D0')
-    plt.scatter(get_x(inc, limit, repeat, -0.5), get_p(pd_WT2, pd_WT, feature, inc, limit, repeat),
-                alpha=0.5, s=5, c='#48D1CC')
-    plt.scatter(get_x(inc, limit, repeat, 0), get_p(pd_WT3, pd_WT, feature, inc, limit, repeat),
-                alpha=0.5, s=5, c='#00CED1')
-    plt.scatter(get_x(inc, limit, repeat, 0.5), get_p(pd_sample, pd_WT, feature, inc, limit, repeat),
-                alpha=0.5, s=5, c='#FF4500')
+    n_middle = (len(ctrl_lst) + 1) // 2
+    pd_WT = data_pd[data_pd['sample'] == 'WT'] if feature != 'circ' else \
+        data_pd[(data_pd['sample'] == 'WT') & (data_pd['size'] > 50)]
+    for i in range(len(ctrl_lst)):
+        pd_ctrl = data_pd[data_pd['sample'] == ctrl_lst[i]] if feature != 'circ' else \
+            data_pd[(data_pd['sample'] == ctrl_lst[i]) & (data_pd['size'] > 50)]
+        plt.scatter(get_x(inc, limit, repeat, (i-n_middle)/2.0),
+                    get_p(pd_ctrl, pd_WT, feature, inc, limit, repeat),
+                    alpha=0.5, s=5, c='#40E0D0', label=ctrl_lst[i])
+
+    pd_sample = data_pd[data_pd['sample'] == sample] if feature != 'circ' else \
+        data_pd[(data_pd['sample'] == sample) & (data_pd['size'] > 50)]
+    plt.scatter(get_x(inc, limit, repeat, (len(ctrl_lst)-n_middle)/2.0),
+                get_p(pd_sample, pd_WT, feature, inc, limit, repeat),
+                alpha=0.5, s=5, c='#FF4500', label=sample)
     plt.plot(x, 0 * x + 5, linestyle='--', color='#696969')
     plt.xlabel('number of traces')
-    plt.ylabel('mobile fraction  -ln(KS p-value)')
-    plt.savefig('%s/%s_%s_p.pdf' % (save_path, sample_name, feature))
+    plt.ylabel('%s -ln(KS p-value)' % feature)
+    plt.savefig('%s%s_%s_p.pdf' % (save_path, sample, feature))
     plt.close()
 
 
@@ -525,5 +563,5 @@ def plot_violin(feature: str, pd_data: pd.DataFrame, save_path: str, sample_name
     """
     plt.figure(figsize=(12, 4), dpi=80)
     sns.violinplot(x='sample', y=feature, data=pd_data, notch=False)
-    plt.savefig('%s/%s_%s.pdf' % (save_path, sample_name, feature))
+    plt.savefig('%s%s_%s.pdf' % (save_path, sample_name, feature))
     plt.close()
